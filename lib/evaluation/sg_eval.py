26d73d3e20e13815ea49bf135f6d0e1668f82c93
wishforgood <616251828@qq.com>
2020-09-06 20:32:26 +0800

    Update sg_eval.py

--- lib/evaluation/sg_eval.py
+++ lib/evaluation/sg_eval.py
@@ -28,10 +28,10 @@ class BasicSceneGraphEvaluator:
         return evaluators
 
     def evaluate_scene_graph_entry(self, gt_entry, pred_scores, viz_dict=None, iou_thresh=0.5):
-        res = evaluate_from_dict(gt_entry, pred_scores, self.mode, self.result_dict,
+        pred_to_gt, pred_5ples, relation_scores, rt_cls, rt_reg = evaluate_from_dict(gt_entry, pred_scores, self.mode, self.result_dict,
                                   viz_dict=viz_dict, iou_thresh=iou_thresh, multiple_preds=self.multiple_preds)
         # self.print_stats()
-        return res
+        return pred_to_gt, pred_5ples, relation_scores, rt_cls, rt_reg
 
     def save(self, fn):
         np.save(fn, self.result_dict)
@@ -56,10 +56,10 @@ def evaluate_from_dict(gt_entry, pred_entry, mode, result_dict, multiple_preds=F
     :param gt_entry: Dictionary containing gt_relations, gt_boxes, gt_classes
     :param pred_entry: Dictionary containing pred_rels, pred_boxes (if detection), pred_classes
     :param mode: 'det' or 'cls'
-    :param result_dict: 
-    :param viz_dict: 
-    :param kwargs: 
-    :return: 
+    :param result_dict:
+    :param viz_dict:
+    :param kwargs:
+    :return:
     """
     gt_rels = gt_entry['gt_relations']
     gt_boxes = gt_entry['gt_boxes'].astype(float)
@@ -114,7 +114,7 @@ def evaluate_from_dict(gt_entry, pred_entry, mode, result_dict, multiple_preds=F
         pred_rels = np.column_stack((pred_rel_inds, 1+rel_scores[:,1:].argmax(1)))
         predicate_scores = rel_scores[:,1:].max(1)
 
-    pred_to_gt, pred_5ples, rel_scores = evaluate_recall(
+    pred_to_gt, pred_5ples, relation_scores, rt_cls, rt_reg = evaluate_recall(
                 gt_rels, gt_boxes, gt_classes,
                 pred_rels, pred_boxes, pred_classes,
                 predicate_scores, obj_scores, phrdet= mode=='phrdet',
@@ -126,7 +126,7 @@ def evaluate_from_dict(gt_entry, pred_entry, mode, result_dict, multiple_preds=F
 
         rec_i = float(len(match)) / float(gt_rels.shape[0])
         result_dict[mode + '_recall'][k].append(rec_i)
-    return pred_to_gt, pred_5ples, rel_scores
+    return pred_to_gt, pred_5ples, relation_scores, rt_cls, rt_reg
 
     # print(" ".join(["R@{:2d}: {:.3f}".format(k, v[-1]) for k, v in result_dict[mode + '_recall'].items()]))
     # Deal with visualization later
@@ -198,7 +198,7 @@ def evaluate_recall(gt_rels, gt_boxes, gt_classes,
         # raise ValueError("Somehow the relations werent sorted properly")
 
     # Compute recall. It's most efficient to match once and then do recall after
-    pred_to_gt = _compute_pred_matches(
+    pred_to_gt, rt_cls, rt_reg = _compute_pred_matches(
         gt_triplets,
         pred_triplets,
         gt_triplet_boxes,
@@ -213,7 +213,7 @@ def evaluate_recall(gt_rels, gt_boxes, gt_classes,
         pred_triplets[:, [0, 2, 1]],
     ))
 
-    return pred_to_gt, pred_5ples, relation_scores
+    return pred_to_gt, pred_5ples, relation_scores, rt_cls, rt_reg
 
 
 def _triplet(predicates, relations, classes, boxes,
@@ -250,47 +250,60 @@ def _triplet(predicates, relations, classes, boxes,
 
 
 def _compute_pred_matches(gt_triplets, pred_triplets,
-                 gt_boxes, pred_boxes, iou_thresh, phrdet=False):
+                          gt_boxes, pred_boxes, iou_thresh, phrdet=False):
     """
     Given a set of predicted triplets, return the list of matching GT's for each of the
     given predictions
-    :param gt_triplets: 
-    :param pred_triplets: 
-    :param gt_boxes: 
-    :param pred_boxes: 
-    :param iou_thresh: 
-    :return: 
+    :param gt_triplets:
+    :param pred_triplets:
+    :param gt_boxes:
+    :param pred_boxes:
+    :param iou_thresh:
+    :return:
     """
     # This performs a matrix multiplication-esque thing between the two arrays
     # Instead of summing, we want the equality, so we reduce in that way
     # The rows correspond to GT triplets, columns to pred triplets
     keeps = intersect_2d(gt_triplets, pred_triplets)
     gt_has_match = keeps.any(1)
+    # print(' ')
+    # print('keeps')
+    # print(keeps)
+    # print('gt_has_match')
+    # print(gt_has_match)
     pred_to_gt = [[] for x in range(pred_boxes.shape[0])]
+    correct_clssified = []
+    correct_regressed = []
     for gt_ind, gt_box, keep_inds in zip(np.where(gt_has_match)[0],
                                          gt_boxes[gt_has_match],
                                          keeps[gt_has_match],
                                          ):
         boxes = pred_boxes[keep_inds]
+        # print('match')
+        # print(keep_inds)
+        # print('box_match')
         if phrdet:
             # Evaluate where the union box > 0.5
             gt_box_union = gt_box.reshape((2, 4))
             gt_box_union = np.concatenate((gt_box_union.min(0)[:2], gt_box_union.max(0)[2:]), 0)
 
             box_union = boxes.reshape((-1, 2, 4))
-            box_union = np.concatenate((box_union.min(1)[:,:2], box_union.max(1)[:,2:]), 1)
+            box_union = np.concatenate((box_union.min(1)[:, :2], box_union.max(1)[:, 2:]), 1)
 
             inds = bbox_overlaps(gt_box_union[None], box_union)[0] >= iou_thresh
 
         else:
-            sub_iou = bbox_overlaps(gt_box[None,:4], boxes[:, :4])[0]
-            obj_iou = bbox_overlaps(gt_box[None,4:], boxes[:, 4:])[0]
+            sub_iou = bbox_overlaps(gt_box[None, :4], boxes[:, :4])[0]
+            obj_iou = bbox_overlaps(gt_box[None, 4:], boxes[:, 4:])[0]
 
             inds = (sub_iou >= iou_thresh) & (obj_iou >= iou_thresh)
 
+        # print(inds)
         for i in np.where(keep_inds)[0][inds]:
             pred_to_gt[i].append(int(gt_ind))
-    return pred_to_gt
+        correct_clssified.append(len(inds))
+        correct_regressed.append(np.sum(inds))
+    return pred_to_gt, correct_clssified, correct_regressed
 
 
 
@@ -358,19 +371,23 @@ def eval_entry(mode, gt_entry, pred_entry, evaluator, evaluator_multiple_preds, 
         gt_entry,
         pred_entry,
     )
-
+    rt_clses = []
+    rt_regs = []
     for (pred_id, _, evaluator_rel), (_, _, evaluator_rel_mp) in zip(evaluator_list, evaluator_multiple_preds_list):
         gt_entry_rel = gt_entry.copy()
         mask = np.in1d(gt_entry_rel['gt_relations'][:, -1], pred_id)
         gt_entry_rel['gt_relations'] = gt_entry_rel['gt_relations'][mask, :]
         if gt_entry_rel['gt_relations'].shape[0] == 0:
             continue
-        
-        evaluator_rel[mode].evaluate_scene_graph_entry(
+
+        pred_to_gt, pred_5ples, relation_scores, rt_cls, rt_reg = evaluator_rel[mode].evaluate_scene_graph_entry(
                 gt_entry_rel,
                 pred_entry,
         )
+        rt_clses.append(rt_cls)
+        rt_regs.append(rt_reg)
         evaluator_rel_mp[mode].evaluate_scene_graph_entry(
                 gt_entry_rel,
                 pred_entry,
         )
+    return rt_clses, rt_regs
